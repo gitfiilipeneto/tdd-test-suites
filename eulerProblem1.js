14264cb7e9c6b3b7beff929c596cf5e1dc2ad65f
Filipe <filipenetopires@hotmail.com>
2021-02-21 11:08:22 -0300

    Euler Problem 1

---
+++ eulerProblem1.js
@@ -0,0 +1,33 @@
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+//The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+//testar %3==0??
+// testar os outputs, no caso a variavel su,
+
+//testar !%5==0
+//testar %3==0 || %5==0
+//
+
+const multiples = (num) => {
+
+    let sum = 0
+    let limit = num
+    for (let i = 0; i < limit; i++) {
+
+        let multiplesOfThree = i % 3 === 0
+        let multiplesOffive = i % 5 === 0
+
+        let bothMultiples = multiplesOfThree && multiplesOfThree
+
+        if (bothMultiples || multiplesOfThree || multiplesOffive) {
+
+            sum = sum + i
+
+        }
+    }
+
+    return sum
+}
+
+module.exports = multiples
---
+++ eulerProblem1.test.js
@@ -0,0 +1,11 @@
+const multiples = require('./eulerProblem1')
+
+test('tests if the primary output with inititial condition', () => {
+    const num = 10
+    expect(multiples(num)).toBe(23)
+})
+
+test('tests inputs will get more multiples, in this case(sum = +10 +13) ', () => {
+    const num = 13
+    expect(multiples(num)).toBe(45)
+})

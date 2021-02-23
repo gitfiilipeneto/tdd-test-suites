da6673ea6ac93e3f1ea529240f3c4c808b0202d7
Filipe <filipenetopires@hotmail.com>
2021-02-23 18:23:37 -0300

    Refactoring the fibonachos function

--- eulerProblem2/eulerProblem2.test.js
+++ eulerProblem2/eulerProblem2.test.js
@@ -1,11 +1,6 @@
 const fibonacci = require('./eulerProblem2')
 
-test('test the initial condition euler problem2', () => {
-    expect(fibonacci(4)).toBe(3) 
+test('test the initial condition euler problem2, doesnt runs with lower than or equals 2 values', () => {
+    expect(fibonacci(1)).toBe(null) 
 })
-
-test('tests if ', () => {
-    
-})
-
 

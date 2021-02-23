8b3c2a69823f0f67283f6f2d888f35adda2e6037
Filipe <filipenetopires@hotmail.com>
2021-02-23 00:17:24 -0300

    started the fibonachos problem

---
+++ eulerProblem2/eulerProblem2.test.js
@@ -0,0 +1,11 @@
+const fibonacci = require('./eulerProblem2')
+
+test('test the initial condition euler problem2', () => {
+    expect(fibonacci(4)).toBe(3) 
+})
+
+test('tests if ', () => {
+    
+})
+
+
